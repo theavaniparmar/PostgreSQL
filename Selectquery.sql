0cead15a6dd48a927bc9c54d3de64d201e01dd19
Avani Parmar <133568468+theavaniparmar@users.noreply.github.com>
2025-02-05 18:35:17 +0530

    changes

--- Selectquery.sql
+++ Selectquery.sql
@@ -1,1 +1,12 @@
+select now();
+select email from customer;
+select  first_name, last_name, email from customer;
 Select first_name|| ' '|| last_name as full_name from customer;
+Select first_name || last_name as full_name from customer;
+
+select first_name, last_name "surname" from customer;
+select first_name, last_name as surname from customer;
+select * from customer;
+select  first_name from customer order by first_name asc;
+select first_name, last_name from customer order by last_name desc;
+select first_name, last_name from customer order by first_name asc, last_name desc;

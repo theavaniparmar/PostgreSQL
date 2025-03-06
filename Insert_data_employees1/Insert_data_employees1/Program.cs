0d0ec36fc4edbb30303e6637a3b36f6efd3c1fdd
theavaniparmar <avniparmar515@gmail.com>
2025-03-06 14:27:22 +0530

    insert record in table

--- Insert_data_employees1/Insert_data_employees1/Program.cs
+++ Insert_data_employees1/Insert_data_employees1/Program.cs
@@ -16,14 +16,40 @@ class Program
         string connString = config.GetConnectionString("PostgresConnection");
 
         //insert employees
-        InsertEmployee(connString, "Avani", "abc@gmail.com", 85000.80);
+        InsertEmployee(connString, "Avani", "abc@gmail.com", 85000);
 
     }
     static void InsertEmployee(string connString, string name, string email, decimal salary)
     {
         using (var conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
 
+            string insertQuery = "insert into employees1(name, email, salary) values (@p_name, @p_email, @p_salary)";
+
+            using (var cmd = new NpgsqlCommand(insertQuery, conn))
+            {
+                cmd.Parameters.AddWithValue("p_name", name);
+                cmd.Parameters.AddWithValue("p_email", email);
+                cmd.Parameters.AddWithValue("p_salary", salary);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    Console.WriteLine($"Employee {name} inserted successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Insertion failed");
+                }
+
+
+            }
+
+        }
     }
+}
 
         
 

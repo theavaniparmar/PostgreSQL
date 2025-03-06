019428f1ad7e73f763da90d72f84afeb77e3b435
theavaniparmar <avniparmar515@gmail.com>
2025-03-06 12:40:08 +0530

    update

---
+++ Insert_data_employees1/Insert_data_employees1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+class Program
+{
+    static void Main()
+    {
+        // Load configuration from appsettings.json
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        string connString = config.GetConnectionString("PostgresConnection");
+
+        //insert employees
+        InsertEmployee(connString, "Avani", "abc@gmail.com", 85000.80);
+
+    }
+    static void InsertEmployee(string connString, string name, string email, decimal salary)
+    {
+        using (var conn = new NpgsqlConnection(connString))
+
+    }
+
+        
+
